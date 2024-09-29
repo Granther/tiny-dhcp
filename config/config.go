e90d76f7743fbcb8bb5986bc7af07151dede6f8e
Grant Herrema <granther321@gmail.com>
2024-09-29 05:07:40 -0400

    Got config writing proof of concept, don't want to do too much with this cause I know it will be an iterative process

--- config/config.go
+++ config/config.go
@@ -1,70 +1,158 @@
-package config
+package main
 
-// Configurations exported
-type Configurations struct {
-	Metal		MetalConfigurations
-	Server		ServerConfigurations
+import (
+	"log"
+	"fmt"
+	"encoding/json"
+	"io/ioutil"
+)
+
+type DHCP struct {
+	SubnetMask		string `json:"subnet_mask"`
+	Router			[]string `json:"router"`
+	TimeServer		[]string `json:"time_server"`
+	NameServer		[]string `json:"name_server"`
+	DNSServer		[]string `json:"dns_server"`
+	LogServer		[]string `json:"log_server"`
+	LeaseLen		int `json:"lease_len"`
+	Hostname		string `json:"hostname"`
+	DomainName		string `json:"domain_name"`
+	IPForwarding	bool `json:"ip_forwarding"`
+	DatagramMTU		int `json:"datagram_mtu"`
+	DefaultTTL		int `json:"default_ttl"`
+	TCPTTL			int `json:"tcp_ttl"`
+	BroadcastAddr	string `json:"broadcase_add"`
+	RouterDiscovery bool `json:"router_discovery"`
+	NTPServer		[]string `json:"ntp_server"`
 }
 
-type MetalConfigurations struct {
-	Port			int
-	ListenAddr		string
-	Interface		string
-	HardwareAddr	string
-}
-
-type ServerConfigurations struct {
-	Subnet		string
-	Gateway		string
-	DNS			string
-	LeaseLen	int
-	IPRange		string
-	ServerAddr	string
+type Server struct {
+	Port 			int `json:"port"`
+	ListenInterface	string `json:"listen_interface"`
+	NumWorkers		int	`json:"num_workers"`
 }
 
 type Config struct {
-	Server struct {
-		Port 			int `json:"port"`
-		ListenInterface	string `json:"listen_interface"`
-		NumWorkers		int	`json:"num_workers"`
-	}
-	DHCP struct {
-
-	}
+	Server Server `json:"server"`
+	DHCP DHCP `json:"dhcp"`
 }
 
-DHCPOptSubnetMask            DHCPOpt = 1   // 4, net.IP
-DHCPOptRouter                DHCPOpt = 3   // n*4, [n]net.IP
-DHCPOptTimeServer            DHCPOpt = 4   // n*4, [n]net.IP
-DHCPOptNameServer            DHCPOpt = 5   // n*4, [n]net.IP
-DHCPOptDNS                   DHCPOpt = 6   // n*4, [n]net.IP
-DHCPOptLogServer             DHCPOpt = 7   // n*4, [n]net.IP
-DHCPOptHostname              DHCPOpt = 12  // n, string
-DHCPOptDomainName            DHCPOpt = 15  // n, string
-DHCPOptIPForwarding          DHCPOpt = 19  // 1, bool
-DHCPOptDatagramMTU           DHCPOpt = 22  // 2, uint16
-DHCPOptDefaultTTL            DHCPOpt = 23  // 1, byte
-DHCPOptInterfaceMTU          DHCPOpt = 26  // 2, uint16
-DHCPOptBroadcastAddr         DHCPOpt = 28  // 4, net.IP
-DHCPOptRouterDiscovery       DHCPOpt = 31  // 1, bool
-DHCPOptStaticRoute           DHCPOpt = 33  // n*8, [n]{net.IP/net.IP} -- note the 2nd is router not mask
-DHCPOptTCPTTL                DHCPOpt = 37  // 1, byte
-DHCPOptNISDomain             DHCPOpt = 40  // n, string
-DHCPOptNISServers            DHCPOpt = 41  // 4*n,  [n]net.IP
-DHCPOptNTPServers            DHCPOpt = 42  // 4*n, [n]net.IP
-DHCPOptVendorOption          DHCPOpt = 43  // n, [n]byte // may be encapsulated.
-DHCPOptRequestIP             DHCPOpt = 50  // 4, net.IP
-DHCPOptLeaseTime             DHCPOpt = 51  // 4, uint32
-DHCPOptExtOptions            DHCPOpt = 52  // 1, 1/2/3
-DHCPOptMessageType           DHCPOpt = 53  // 1, 1-7
-DHCPOptServerID              DHCPOpt = 54  // 4, net.IP
-DHCPOptParamsRequest         DHCPOpt = 55  // n, []byte
-DHCPOptMessage               DHCPOpt = 56  // n, 3
-DHCPOptMaxMessageSize        DHCPOpt = 57  // 2, uint16
-DHCPOptT1                    DHCPOpt = 58  // 4, uint32
-// Figure 3-8 Renewing an IP address lease. When the lease reaches 50% (T1) of its validity period, the DHCP client unicasts a DHCP Request message to the DHCP server to request lease renewal. If the server renews the lease (counted from 0), it sends a DHCP ACK message to the client.
-DHCPOptT2                    DHCPOpt = 59  // 4, uint32
-DHCPOptClassID               DHCPOpt = 60  // n, []byte
-DHCPOptClientID              DHCPOpt = 61  // n >=  2, []byte
-DHCPOptDomainSearch          DHCPOpt = 119 // n, string
-DHCPOptClasslessStaticRoute  DHCPOpt = 121 //
+// func ReadConfig(configFile string) (*Config) {
+
+// }
+
+func WriteConfig(writePath string, config *Config) error {
+    jsonData, err := json.MarshalIndent(config, "", "    ")
+    if err != nil {
+        log.Fatalf("Error marshaling to JSON, %v", err)
+		return err
+	}
+
+	writePath = writePath + "config.json"
+	err = ioutil.WriteFile(writePath, jsonData, 0666)
+	if err != nil {
+		return fmt.Errorf("Error writing marshalled JSON to config file: %w", err)
+	}	
+
+	// // Output the JSON string
+    // fmt.Println(string(jsonData))
+
+	return nil
+}
+
+func GetDefaultConfig() *Config {
+	return &Config{
+        Server: Server{
+            Port: 100,
+            ListenInterface: "any",
+            NumWorkers: 10,
+        },
+        DHCP: DHCP {
+            SubnetMask: "255.255.255.0",
+            Router: []string{"192.168.1.1"},
+			TimeServer:		[]string{},
+			NameServer:		[]string{},
+			DNSServer:		[]string{},
+			LogServer:		[]string{},
+			LeaseLen:		30000,
+			Hostname:		"gdhcp",
+			DomainName:		"local",
+			IPForwarding:	true,
+			DatagramMTU:	1500,
+			DefaultTTL:		254,
+			TCPTTL:			254,     
+			BroadcastAddr:	"255.255.255.255",
+			RouterDiscovery: true,
+			NTPServer:		[]string{},
+        },
+    }
+}
+
+func main() {
+	config := GetDefaultConfig()
+	WriteConfig("./", config)
+}
+
+
+// DHCPOptSubnetMask            DHCPOpt = 1   // 4, net.IP
+// DHCPOptRouter                DHCPOpt = 3   // n*4, [n]net.IP
+// DHCPOptTimeServer            DHCPOpt = 4   // n*4, [n]net.IP
+// DHCPOptNameServer            DHCPOpt = 5   // n*4, [n]net.IP
+// DHCPOptDNS                   DHCPOpt = 6   // n*4, [n]net.IP
+// DHCPOptLogServer             DHCPOpt = 7   // n*4, [n]net.IP
+// DHCPOptLeaseTime             DHCPOpt = 51  // 4, uint32
+// DHCPOptHostname              DHCPOpt = 12  // n, string
+// DHCPOptDomainName            DHCPOpt = 15  // n, string
+// DHCPOptIPForwarding          DHCPOpt = 19  // 1, bool
+// DHCPOptDatagramMTU           DHCPOpt = 22  // 2, uint16
+// DHCPOptDefaultTTL            DHCPOpt = 23  // 1, byte
+// DHCPOptTCPTTL                DHCPOpt = 37  // 1, byte
+// DHCPOptBroadcastAddr         DHCPOpt = 28  // 4, net.IP
+// DHCPOptRouterDiscovery       DHCPOpt = 31  // 1, bool
+// DHCPOptNTPServers            DHCPOpt = 42  // 4*n, [n]net.IP
+
+// DHCPOptVendorOption          DHCPOpt = 43  // n, [n]byte // may be encapsulated.
+
+// DHCPOptRequestIP             DHCPOpt = 50  // 4, net.IP
+// DHCPOptExtOptions            DHCPOpt = 52  // 1, 1/2/3
+// DHCPOptMessageType           DHCPOpt = 53  // 1, 1-7
+// DHCPOptParamsRequest         DHCPOpt = 55  // n, []byte
+// DHCPOptMessage               DHCPOpt = 56  // n, 3
+// // DHCPOptT1                    DHCPOpt = 58  // 4, uint32
+// // // Figure 3-8 Renewing an IP address lease. When the lease reaches 50% (T1) of its validity period, the DHCP client unicasts a DHCP Request message to the DHCP server to request lease renewal. If the server renews the lease (counted from 0), it sends a DHCP ACK message to the client.
+// // DHCPOptT2                    DHCPOpt = 59  // 4, uint32
+
+// // Non config
+// DHCPOptClassID               DHCPOpt = 60  // n, []byte
+// DHCPOptClientID              DHCPOpt = 61  // n >=  2, []byte
+// DHCPOptDomainSearch          DHCPOpt = 119 // n, string
+
+// // Work
+// DHCPOptClasslessStaticRoute  DHCPOpt = 121 //
+// // DHCPOptInterfaceMTU          DHCPOpt = 26  // 2, uint16 Can get auto?
+// DHCPOptServerID              DHCPOpt = 54  // 4, net.IP
+// DHCPOptStaticRoute           DHCPOpt = 33  // n*8, [n]{net.IP/net.IP} -- note the 2nd is router not mask
+// DHCPOptMaxMessageSize        DHCPOpt = 57  // 2, uint16 Set to interface mtu
+
+
+// Configurations exported
+// type Configurations struct {
+// 	Metal		MetalConfigurations
+// 	Server		ServerConfigurations
+// }
+
+// type MetalConfigurations struct {
+// 	Port			int
+// 	ListenAddr		string
+// 	Interface		string
+// 	HardwareAddr	string
+// }
+
+// type ServerConfigurations struct {
+// 	Subnet		string
+// 	Gateway		string
+// 	DNS			string
+// 	LeaseLen	int
+// 	IPRange		string
+// 	ServerAddr	string
+// }
--- test.go
+++ test.go
@@ -1,22 +1,89 @@
 package main
 
 import (
-	"fmt"
+	"log"
+    "fmt"
+    "encoding/json"
 
-    "github.com/spf13/viper"
+    // "github.com/spf13/viper"
 )
 
-func loadConfig(filename string) (map[string]interface{}, error) {
-    viper.SetConfigFile(filename)
-    err := viper.ReadInConfig()
-    if err != nil {
-        return nil, err
-    }
+// func loadConfig(filename string) (map[string]interface{}, error) {
+//     viper.SetConfigFile(filename)
+//     err := viper.ReadInConfig()
+//     if err != nil {
+//         return nil, err
+//     }
 
-    return viper.AllSettings(), nil
+//     return viper.AllSettings(), nil
+// }
+
+// func main() {
+// 	x, _ := loadConfig("./config.yml")
+// 	fmt.Println(x["metal"]["port"])
+// }
+
+type Server struct {
+    Port 			int `json:"port"`
+    ListenInterface	string `json:"listen_interface"`
+    NumWorkers		int	`json:"num_workers"`
+}
+
+type DHCP struct {
+    SubnetMask		string `json:"subnet_mask"`
+    Router			[]string `json:"router"`
+}
+
+type Config struct {
+    Server Server `json:"server"`
+    DHCP   DHCP `json:"dhcp"`
 }
 
 func main() {
-	x, _ := loadConfig("./config.yml")
-	fmt.Println(x["metal"]["port"])
+	// viper.SetConfigName("config") // Name of the file without extension
+    // viper.SetConfigType("json")   // File format (json, yaml, etc.)
+    // viper.AddConfigPath(".")      // Path to look for the file
+
+    // // Read the config file
+    // if err := viper.ReadInConfig(); err != nil {
+    //     log.Fatalf("Error reading config file, %s", err)
+    // }
+
+    // // Create a config struct
+    // var config Config
+
+    // // Unmarshal the JSON data into the struct
+    // err := viper.Unmarshal(&config)
+    // if err != nil {
+    //     log.Fatalf("Unable to unmarshal config, %v", err)
+    // }
+
+    config := Config{
+        Server: Server{
+            Port: 100,
+            ListenInterface: "any",
+            NumWorkers: 100,
+        },
+        DHCP: DHCP {
+            SubnetMask: "255.255.255.0",
+            Router: []string{"192.168.1.1"},
+        },
+    }
+
+
+    jsonData, err := json.MarshalIndent(config, "", "    ")
+    if err != nil {
+        log.Fatalf("Error marshaling to JSON, %v", err)
+    }
+
+    // Output the JSON string
+    fmt.Println(string(jsonData))
+    // Marshal 
+
+    // Output the config struct values
+    // fmt.Printf("Server: %s\n", config.Server)
+    // fmt.Printf("Port: %d\n", config.Port)
+    // fmt.Printf("Database User: %s\n", config.Database.User)
+    // fmt.Printf("Database Password: %s\n", config.Database.Password)
+    // fmt.Printf("Database Name: %s\n", config.Database.DBName)
 }
