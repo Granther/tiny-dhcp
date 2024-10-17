c49d0ed5dc8f8f895357c8d283ccf7f20585534c
Grant Herrema <granth@withnubo.com>
2024-10-17 02:56:41 -0400

    Implemented circular cache for rotating pool, impl start of leases cache datastructure

---
+++ src/cache/addrQueue.go
@@ -0,0 +1,102 @@
+package queue
+
+import (
+	"fmt"
+)
+
+type ListNode struct {
+	val		net.IP
+	prev	*ListNode
+	next	*ListNode
+}
+
+type CircularQueue struct {
+	space		int
+	left		*ListNode
+	right		*ListNode
+}
+
+func NewListNode(val net.IP, prev *ListNode, next *ListNode) *ListNode {
+	return &ListNode{
+		val:	val,
+		prev:	prev,
+		next: 	next,
+	}
+}
+
+func NewCirularQueue(max int) *CircularQueue {
+	// Doubley linked, 2 dummy nodes linked to eachother
+	left := NewListNode(0, nil, nil)
+	right := NewListNode(0, left, nil)
+	left.next = right
+
+	return &CircularQueue{
+		space:		max,
+		left: 		left,
+		right:		right,
+	}
+}
+
+func (q *CircularQueue) isEmpty() bool {
+	return q.left.next == q.right
+}
+
+func (q *CircularQueue) isFull() bool {
+	return q.space == 0
+}
+
+func (q *CircularQueue) Front() net.IP {
+	if q.isEmpty() {
+		return -1
+	}
+	return q.left.next.val
+}
+
+func (q *CircularQueue) Rear() net.IP {
+	if q.isEmpty() {
+		return -1
+	}
+	return q.right.prev.val
+}
+
+func (q *CircularQueue) enQueue(val net.IP) bool {
+	if q.isFull() {
+		return false
+	}
+	newNode := NewListNode(val, q.right.prev, q.right)
+	q.right.prev.next = newNode
+	q.right.prev = newNode
+	q.space -= 1
+
+	return true
+}
+
+func (q *CircularQueue) deQueue() bool {
+	if q.isEmpty() {
+		return false
+	}
+	q.left.next = q.left.next.next
+	q.left.next.prev = q.left
+	q.space += 1
+
+	return true
+}
+
+func (q *CircularQueue) RearToEnd() {
+	node := q.left.next
+	// Fix left
+	q.left.next = node.next
+	q.left.next.prev = q.left
+	// Fix Node
+	node.next = q.right
+	node.prev = q.right.prev
+	// Fix right
+	q.right.prev.next = node
+	q.right.prev = node
+}
+
+func (q *CircularQueue) PrintQueue() {
+	for ptr := q.left.next; ptr != q.right; ptr = ptr.next {
+		fmt.Println(ptr.val)
+	}
+}
---
+++ src/cache/leaseCache.go
@@ -0,0 +1,47 @@
+package cache 
+
+import (
+	"fmt"
+	"time"
+	"net"
+)
+
+// Have a queue of available addrs
+// When static addr is encountered, add to end of queue
+// At runtime queue of N addrs is created
+// If queue is empty, add additionalt pool of N available addrs (if possible)
+
+type LeasesCache struct {
+	ipCache			map[net.IP]*LeaseNode
+	macCache		map[net.HardwareAddr]*LeaseNode
+	availableQueue	*CircularQueue
+}
+
+type LeaseNode struct {
+	ip			net.IP
+	mac			net.HardwareAddr
+	leaseLen	time.Duration
+	leasedOn	time.Time
+}
+
+func NewLeaseNode(ip net.IP, mac net.HardwareAddr, leaseLen time.Duration, leasedOn time.Time) *LeaseNode {
+	return &LeaseNode {
+		ip:			ip,
+		mac:		mac,
+		leaseLen:	leaseLen,
+		leasedOn:	leasedOn,
+	}
+}
+
+func NewLeasesCache(max int) *LeasesCache {
+	ipCache := make(map[net.IP]*LeaseNode)
+	macCache := make(map[net.IP]*LeaseNode)
+	availableQueue := NewCirularQueue(max)
+
+	return &LeasesCache {
+		ipCache: 		ipCache,
+		macCache:		macCache,
+		availableQueue:	availableQueue,
+	}
+}
+
